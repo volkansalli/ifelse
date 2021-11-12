875222b1c4f786557ececd1b28ffa1f9d6cefca7
volkan_salli <volkan_salli@hotmail.com>
2021-11-12 13:49:03 +0300

    Add files via upload

---
+++ ifelse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time=Convert.ToInt32(Console.ReadLine()); //DateTime.Now.Hour;
+            if(time >= 6 && time <=11)
+            Console.WriteLine("Günaydın");
+            else if(time < 18 && time > 6 )
+            Console.WriteLine("İyi günler");
+            else
+            Console.WriteLine("iyi geceler");
+
+
+        }
+    }
+}
